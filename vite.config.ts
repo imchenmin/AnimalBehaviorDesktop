83b90894d03b4dd0f0c1f4c37d8c3bbd7879e144
chenminken <zbcmb@outlook.com>
2022-09-01 08:01:31 +0800

    add electron support to vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
+import { rmSync } from 'fs'
 import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import electron, { onstart } from 'vite-plugin-electron'
+
 
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
@@ -15,12 +18,13 @@ import {
 } from 'unocss'
 
 const pathSrc = path.resolve(__dirname, 'src')
+rmSync('dist', { recursive: true, force: true }) // v14.14.0
 
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
-      '~/': `${pathSrc}/`,
+      '~/': `${pathSrc}/`
     },
   },
   css: {
@@ -32,6 +36,36 @@ export default defineConfig({
   },
   plugins: [
     vue(),
+    electron({
+      main: {
+        entry: 'electron/main/index.ts',
+        vite: {
+          build: {
+            // For Debug
+            sourcemap: true,
+            outDir: 'dist/electron/main',
+          },
+          // Will start Electron via VSCode Debug
+          plugins: [process.env.VSCODE_DEBUG ? onstart() : null],
+        },
+      },
+      preload: {
+        input: {
+          // You can configure multiple preload here
+          index: path.join(__dirname, 'electron/preload/index.ts'),
+        },
+        vite: {
+          build: {
+            // For Debug
+            sourcemap: 'inline',
+            outDir: 'dist/electron/preload',
+          },
+        },
+      },
+      // Enables use of Node.js API in the Renderer-process
+      // https://github.com/electron-vite/vite-plugin-electron/tree/main/packages/electron-renderer#electron-renderervite-serve
+      renderer: {},
+    }),
     Components({
       // allow auto load markdown components under `./src/components/`
       extensions: ['vue', 'md'],
@@ -62,4 +96,8 @@ export default defineConfig({
       ]
     }),
   ],
+  server: process.env.VSCODE_DEBUG ? {
+    host: "127.0.0.1",
+    port: 3000,
+  } : undefined,
 })
