343363059b0bdd8aeb27c74d85f08234745a8157
Louisoix <111611129@mail.sustech.edu.cn>
2022-09-07 13:13:45 +0800

    commit camera vue

--- src/components/Camera.vue
+++ src/components/Camera.vue
@@ -17,11 +17,10 @@
 
     <el-form :model="record">
         <el-form-item>
-            <el-button type="primary" @click="handleOpen">打开相机</el-button>
-            <el-button type="primary" @click="handleStart">开始录制</el-button>
-            <el-button type="primary" @click="handleStop">结束录制</el-button>
-            <el-button type="primary" @click="handleClose">关闭相机</el-button>
-            <el-button>Cancel</el-button>
+            <el-button type="primary" @click="handleOpen" v-if="cameraflag">打开相机</el-button>
+            <el-button type="primary" @click="handleClose" v-else>关闭相机</el-button>
+            <el-button type="primary" @click="handleStart" v-if="!cameraflag && recordflag">开始录制</el-button>
+            <el-button type="primary" @click="handleStop" v-if="!cameraflag && ! recordflag">关闭录制</el-button>
         </el-form-item>
     </el-form>
 
@@ -37,6 +36,8 @@ export default {
         genotype: '',
         datetime: '',
         current_exp_id: '',
+        cameraflag: true,
+        recordflag: true
     }),
     computed: {
         current_exp() {
@@ -47,14 +48,16 @@ export default {
     },
     methods: {
         handleOpen() {
-            fetch('http://127.0.0.1:5001/api/open_camera', {
+                fetch('http://127.0.0.1:5001/api/open_camera', {
                 method: 'post',
                 body: JSON.stringify({ a: 1 }),
                 headers: {
                     'Content-Type': 'application/json'
                 }
             }).then(function (data) {
+                
             })
+            this.cameraflag = false
         },
         handleStart() {
             fetch('http://127.0.0.1:5001/api/start_record', {
@@ -64,28 +67,32 @@ export default {
                     'Content-Type': 'application/json'
                 }
             }).then(function (data) {
-
+                
             })
+            this.recordflag = false
         },
         handleStop() {
             fetch('http://127.0.0.1:5001/api/stop_record', {
                 method: 'post',
-                body: JSON.stringify({ a: 1 }),
+                body: JSON.stringify({ video_filename: this.current_exp.folder_path }),
                 headers: {
                     'Content-Type': 'application/json'
                 }
             }).then(function (data) {
+                
             })
+            this.recordflag = true
         },
         handleClose() {
             fetch('http://127.0.0.1:5001/api/close_camera', {
                 method: 'post',
-                body: JSON.stringify({ a: 1 }),
+                body: JSON.stringify({ video_filename: this.current_exp.folder_path }),
                 headers: {
                     'Content-Type': 'application/json'
                 }
             }).then(function (data) {
             })
+            this.cameraflag = true
         }
     },
 }
